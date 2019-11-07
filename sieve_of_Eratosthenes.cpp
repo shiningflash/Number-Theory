a721aeae3c5955123d48a60d784b263ce81d97c0
Amirul Islam <35567854+shiningflash@users.noreply.github.com>
2019-11-07 18:14:50 +0600

    Update sieve_of_Eratosthenes.cpp

--- sieve_of_Eratosthenes.cpp
+++ sieve_of_Eratosthenes.cpp
@@ -1,12 +1,18 @@
+/*
+    @author: Amirul Islam
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
 
 const int mx = 1e5;
+
 int a[mx+4];
 
+// a[0] == 0, prime number
+// a[1] == 1, not prime
 void sieve() {
-	a[0] = a[1] = 1;
+    a[0] = a[1] = 1;
     for (int i = 2; i*i <= mx; i++) {
         if (!a[i]) {
             for (int j = i<<1; j <= mx; j+=i) a[j] = 1;
@@ -16,8 +22,6 @@ void sieve() {
 
 int main() {
     //freopen("in", "r", stdin);
-    //ios_base::sync_with_stdio(0);
-    //cin.tie(NULL);
     
     sieve();
     int n;
