604ca2be714aaf5e3dcb33c9f58ebbd572202b4a
Amirul Islam <35567854+shiningflash@users.noreply.github.com>
2020-07-15 20:38:20 +0600

    Update 2.0

--- extended_euclidean_algorithm.cpp
+++ extended_euclidean_algorithm.cpp
@@ -10,20 +10,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int gcdExtended(int a, int b, int &x, int &y) {
+int gcdExtended(int a, int b, int *x, int *y) {
     if (b == 0) {
-        x = 1;
-        y = 0;
+        *x = 1;
+        *y = 0;
         return a;
     }
     int x1, y1;
-    int gcd = gcdExtended(b, a % b, x1, y1);
-    x = y1;
-    y = x1 - y1 * (a / b);
+    int gcd = gcdExtended(b, a % b, &x1, &y1);
+    *x = y1;
+    *y = x1 - y1 * (a / b);
     return  gcd;
 }
 
 int main() {
     int a = 161, b = 28, x, y;
-    cout << gcdExtended(a, b, x, y) << endl;
+    cout << gcdExtended(a, b, &x, &y) << endl;
 }
